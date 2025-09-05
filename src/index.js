b1db97d5de5534d33d96d0495e1fca1d2ac5bd9b
Ch-SaifUllah-Sundhu <chsaifullah17517@gmail.com>
2025-09-06 02:16:44 +0500

    connect to mongo db

---
+++ src/index.js
@@ -0,0 +1,46 @@
+//require("dotenv").config({path});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+});
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import exprees from "express";
+const app = exprees();
+
+(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+       app.on("error", (err)=>{
+        console.error("Error",err);
+        throw err
+       })
+       app.listen(process.env.PORT,()=>{
+        console.log(`Server started on port ${process.env.PORT}`);
+       })
+    }catch (error) {
+        console.error("Error",error);
+        throw err
+    }
+})()
+    */
