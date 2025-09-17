e5f5f20889a5723ef2f2681124e0f7def5e87beb
Ch-SaifUllah-Sundhu <chsaifullah17517@gmail.com>
2025-09-18 03:57:32 +0500

    fix bugs

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import expree from "express";
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = expree();
+
+app.use(helmet());
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+}));
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
@@ -15,9 +23,16 @@ app.use(expree.urlencoded({ extended: true , limit : "20kb"}));
 app.use(expree.static("public"));
 
 //routes
-
 import userRoutes from "./routes/user.routes.js"
-//routes declaration
 app.use("/api/v1/users", userRoutes)
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error"
+    });
+});
 
 export { app }
