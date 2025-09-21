043aab72b0a0014aa8dd727fefce172db5e808d8
Ch-SaifUllah-Sundhu <chsaifullah17517@gmail.com>
2025-09-21 23:48:05 +0500

    add user account management

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -219,6 +219,46 @@ const updateAccount = asyncHandler(async(req, res) =>{
     );
 });
 
+const updateAvatar = asyncHandler(async(req, res) =>{
+    const avatarLocalPath = req.file?.path;
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is required")
+    }
+    const avatar = await uploadResult(avatarLocalPath)
+    if(!avatar?.url){
+        throw new ApiError(500, "Error while uploading avatar")
+    }
+    const user = await User.findByIdAndUpdate(req.user._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        { new: true }
+        ).select("-password -refreshToken")
+    return res.status(200).json(new ApiResponse(200, user, "Avatar updated successfully"))
+})
+
+const updateCoverImage = asyncHandler(async(req, res) =>{
+    const coverImageLocalPath = req.file?.path;
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "Cover image file is required")
+    }
+    const coverImage = await uploadResult(coverImageLocalPath)
+    if(!coverImage?.url){
+        throw new ApiError(500, "Error while uploading cover image")
+    }
+    const user = await User.findByIdAndUpdate(req.user._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        { new: true }
+        ).select("-password -refreshToken")
+    return res.status(200).json(new ApiResponse(200, user, "Cover image updated successfully"))
+})
+
 export { 
     registerUser,
     loginUser,
@@ -227,4 +267,6 @@ export {
     ,changeCurrentPassword
     ,getProfile
     ,updateAccount
+    ,updateAvatar
+    ,updateCoverImage
 };
