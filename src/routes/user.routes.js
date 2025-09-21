f32d6e7a38ad00a8f72f92c8ce8a7374bb8bf269
Ch-SaifUllah-Sundhu <chsaifullah17517@gmail.com>
2025-09-22 00:59:00 +0500

    anything

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword , getProfile, updateAccount, updateAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middlewae.js";
+import { get } from "mongoose";
 
 const router = Router();
 
@@ -12,10 +13,17 @@ router.route("/register").post(
     ]),
     registerUser
 )
-
 router.route("/login").post(loginUser)
-
 router.route("/Logout").post( verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post( verifyJWT, changeCurrentPassword)
+router.route("/getProfile").get( verifyJWT, getProfile)
+router.route("/updateAccount").patch( verifyJWT, updateAccount)
+router.route("/Avatar").patch( verifyJWT, upload.single('avatar'), updateAvatar)
+router.route("/CoverImage").patch( verifyJWT, upload.single('coverImage'), updateCoverImage)
+router.route("/c/:username").get( verifyJWT, getUserChannelProfile)
+router.route("/watchHistory").get( verifyJWT, getWatchHistory)
+
+
 
 export default router;
